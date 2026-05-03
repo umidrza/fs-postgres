e1015145d6d55917f6d2cc9ef4cd6648ee9313dc
Umid Rza <umidrza4@gmail.com>
2026-05-03 11:20:21 +0400

    add year to blog

--- models/blog.js
+++ models/blog.js
@@ -24,6 +24,20 @@ Blog.init({
         type: DataTypes.INTEGER,
         allowNull: false,
         defaultValue: 0
+    },
+    year: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate: {
+            min: {
+                args: [1991],
+                msg: 'Year must be at least 1991'
+            },
+            max: {
+                args: [new Date().getFullYear()],
+                msg: 'Year cannot be in the future'
+            }
+        }
     }
 }, {
     sequelize,
